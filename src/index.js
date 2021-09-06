d3e7b442dfa77600f0f07daa773021d913336d1f
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2021-09-06 18:44:33 +0300

    ULMS-1481 Added update method for dispatcher

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { Conference } from './conference.js'
 import { Dispatcher } from './dispatcher.js'
 import { Event } from './event.js'
+import { FetchHttpClient } from './http-client.js'
 import { Telemetry } from './telemetry.js'
 import { Tenant } from './tenant.js'
 
-export { Conference, Dispatcher, Event, Telemetry, Tenant }
+export {
+  Conference,
+  Dispatcher,
+  Event,
+  FetchHttpClient,
+  Telemetry,
+  Tenant
+}
