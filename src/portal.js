3b4a045a97c003fd44dba9cf23f8f6d484d66299
Alex Matveev <ay.flyer@gmail.com>
2022-04-20 16:38:56 +0300

    portal client (#59)
    * feat: portal client
    
    * version 5.3.0
    
    * prettier

---
+++ src/portal.js
@@ -0,0 +1,13 @@
+import BasicClient from './basic-client'
+
+class Portal extends BasicClient {
+  /**
+   * Get portal options
+   * @returns {Promise}
+   */
+  getOptions() {
+    return this.get(this.baseUrl)
+  }
+}
+
+export default Portal
