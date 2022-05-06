f5712f51f2c7141fe41de924d1245af0b8358e5c
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2022-05-06 10:57:36 +0300

    ULMS-1862 Added Presence and PresenceWS clients

--- src/error.js
+++ src/error.js
@@ -26,3 +26,34 @@ export class TimeoutError extends Error {
     this.name = 'TimeoutError'
   }
 }
+
+export class PresenceError extends Error {
+  constructor(...args) {
+    super(...args)
+
+    this.name = 'PresenceError'
+  }
+
+  static get types() {
+    return {
+      ACCESS_DENIED: 'ACCESS_DENIED',
+      AGENT_AUTH_TIMED_OUT: 'AGENT_AUTH_TIMED_OUT',
+      AGENT_REPLACED: 'AGENT_REPLACED',
+      CONNECTION_FAILED: 'CONNECTION_FAILED',
+      DATABASE_QUERY_FAILED: 'DATABASE_QUERY_FAILED',
+      NOT_CONNECTED: 'NOT_CONNECTED',
+      SERIALIZATION_FAILED: 'SERIALIZATION_FAILED',
+      UNAUTHENTICATED: 'UNAUTHENTICATED',
+      UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+      UNSUPPORTED_REQUEST: 'UNSUPPORTED_REQUEST',
+      WS_ERROR: 'WS_ERROR',
+    }
+  }
+
+  static fromType(type) {
+    const errorType =
+      PresenceError.types[type] || PresenceError.types.UNKNOWN_ERROR
+
+    return new PresenceError(errorType)
+  }
+}
