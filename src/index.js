ab649de61038a46bccd5e1d759f710c3b322903c
Aleksey Konstantinov <a.konstantinov@netology-group.ru>
2022-05-11 12:49:23 +0300

    ULMS-1812 Added FVS api client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as Conference } from './conference'
 export { default as Dispatcher } from './dispatcher'
 export { default as Event } from './event'
+export { default as FVS } from './fvs'
 export { default as FetchHttpClient } from './http-client'
 export { default as HTTPEvent } from './http-event'
 export { default as Presence } from './presence'
