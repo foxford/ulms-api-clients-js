309314d88a986163fb01c3b459f1e4c4d3dbc766
Vladimir Berdnikov <vs.berdnikov@mail.ru>
2024-04-10 17:55:05 +0300

    Added new methods to fvs

--- src/fvs.js
+++ src/fvs.js
@@ -2,7 +2,7 @@ import BasicClient from './basic-client'
 
 class FVS extends BasicClient {
   /**
-   * Get issue types
+   * Get issue types in Minigroup
    * @returns {Promise}
    */
   getIssueTypes() {
@@ -10,7 +10,7 @@ class FVS extends BasicClient {
   }
 
   /**
-   * Get issue types
+   * Create issue in Minigroup
    * @param {string} scope
    * @param {number} typeId
    * @param {string} description
@@ -24,6 +24,32 @@ class FVS extends BasicClient {
     }
 
     return this.post(`${this.baseUrl}/ulms-issues/issues`, payload)
+  }
+
+  /**
+   * Get issue categories in Webinar
+   * @returns {Promise}
+   */
+  getIssueCategories() {
+    return this.get(`${this.baseUrl}/api/livestreaming/ulms/issueCategories`)
+  }
+
+  /**
+   * @typedef {Object} WebinarIssue
+   * @property {string} description
+   * @property {boolean} isCutVideo
+   * @property {boolean} isNeedHelp
+   * @property {string} scope
+   * @property {number} subcategoryId
+   * /
+
+  /**
+   * Create issue in Webinar
+   * @param {WebinarIssue} payload
+   * @returns {Promise}
+   */
+  createWebinarIssue(payload) {
+    return this.post(`${this.baseUrl}/api/livestreaming/ulms/issues`, payload)
   }
 }
 
